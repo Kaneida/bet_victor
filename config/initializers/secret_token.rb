604177c3057eaa34e5db9eaab7bc56f84dd176ad
Hao Liu <leomayleomay@gmail.com>
2013-10-15 23:22:50 +0800

    1, Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BetVictor::Application.config.secret_token = 'be345d3803eb408db3de01cb79e2afdba864c2e8f1a7a3bfccb798b4eb730616e3b423f4ed30c6a34570c738eeed862691afbfd87e3e68eda28657d8c6735e0b'
